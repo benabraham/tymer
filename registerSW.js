2fd576dbeb1c389a309d615195341c0f3db29e8a
benabraham <benabraham@users.noreply.github.com>
2024-10-31 11:28:33 +0000

    Deploying to gh-pages from @ benabraham/tymer@90cfc9f32244cc7aa33c1c45d3d86802fc7a5127 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tymer/sw.js', { scope: '/tymer/' })})}
