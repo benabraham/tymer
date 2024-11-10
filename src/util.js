fbbb5bfdb5e80977583bc1f01cc8ded1081827e5
DanielSrb <srb.dan@gmail.com>
2024-11-10 10:43:59 +0100

    less verbose logging plus refactor

--- src/util.js
+++ src/util.js
@@ -1,21 +1,21 @@
 export const log = (text, variable, variant = 0, border = false) => {
   // check if variant is a number and within range and default to 0 if not
   const variantColors = [
-    { color: 'black', background: 'white' },
-    { color: 'white', background: 'black' },
-    { color: 'white', background: 'darkred' },
-    { color: 'white', background: 'green' },
-    { color: 'white', background: 'blue' },
-    { color: 'white', background: 'maroon' },
-    { color: 'white', background: 'purple' },
-    { color: 'white', background: 'olive' },
-    { color: 'white', background: 'navy' },
-    { color: 'white', background: 'teal' },
-    { color: 'black', background: 'orange' },
-    { color: 'black', background: 'gray' },
-    { color: 'black', background: 'skyblue' },
-    { color: 'black', background: 'goldenrod' },
-    { color: 'black', background: 'silver' },
+    { color: 'black', background: 'white' },     // 0
+    { color: 'white', background: 'black' },     // 1
+    { color: 'white', background: 'darkred' },   // 2
+    { color: 'white', background: 'green' },     // 3
+    { color: 'white', background: 'blue' },      // 4
+    { color: 'white', background: 'maroon' },    // 5
+    { color: 'white', background: 'purple' },    // 6
+    { color: 'white', background: 'olive' },     // 7
+    { color: 'white', background: 'navy' },      // 8
+    { color: 'white', background: 'teal' },      // 9
+    { color: 'black', background: 'orange' },    // 10
+    { color: 'black', background: 'gray' },      // 11
+    { color: 'black', background: 'skyblue' },   // 12
+    { color: 'black', background: 'goldenrod' }, // 13
+    { color: 'black', background: 'silver' },    // 14
   ]
 
   if (typeof variant !== 'number' || variant < 0 || variant > variantColors.length - 1) variant = 0
@@ -25,7 +25,7 @@ export const log = (text, variable, variant = 0, border = false) => {
     `
       color: ${variantColors[variant].color};
       background-color: ${variantColors[variant].background};
-      padding: 4px 4px;
+      padding: 4px;
       font-weight: bold;
       ${border ? `border: 2px solid ${variantColors[variant].color};` : ''}
       `,
@@ -33,6 +33,5 @@ export const log = (text, variable, variant = 0, border = false) => {
     `  started: ${variable.timestampStarted === null ? null : new Date(variable.timestampStarted).toLocaleTimeString('cs-CZ', { second: "2-digit", fractionalSecondDigits: 2 })}`,
     `   paused: ${variable.timestampPaused === null ? null : new Date(variable.timestampPaused).toLocaleTimeString('cs-CZ', { second: "2-digit", fractionalSecondDigits: 2 })}`,
   )
-
-  console.table(variable.periods)
+  // console.table(variable.periods)
 }
