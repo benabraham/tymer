23e3493df76d647995f09845911fb2eeb0414638
DanielSrb <srb.dan@gmail.com>
2025-03-13 12:03:33 +0100

    better button logic

--- src/components/timer/controls/timer-controls.jsx
+++ src/components/timer/controls/timer-controls.jsx
@@ -23,70 +23,75 @@ export const TimerControls = () => {
     }
 
     return (
-        <section class="controls">
-            <button
-                onClick={moveToPreviousPeriod}
-                disabled={
-                    timerHasFinished.value
-                    || timerState.value.currentPeriodIndex === null
-                    || timerState.value.currentPeriodIndex === 0
-                }
-            >
-                ⏮️
-            </button>
+        <>
+            <section class="controls">
+                <button
+                    onClick={resetTimer}
+                    disabled={
+                        (initialState.timerDuration === timerDuration.value
+                            && !timerState.value.timestampStarted
+                            && timerDurationRemaining.value !== 0)
+                        || timerState.value.currentPeriodIndex === null
+                    }
+                    class={timerHasFinished.value ? 'highlighted' : ''}
+                >
+                    🔁 Reset
+                </button>
+                <button
+                    onClick={handleStartPause}
+                    disabled={timerHasFinished.value || !timerDurationRemaining.value}
+                >
+                    {timerState.value.runningIntervalId
+                        ? '⏸️️'
+                        : timerState.value.timestampPaused
+                          ? '▶️️'
+                          : '▶️️ Start'}
+                </button>
 
-            <button
-                onClick={handleStartPause}
-                disabled={timerHasFinished.value || !timerDurationRemaining.value}
-            >
-                {timerState.value.runningIntervalId
-                    ? '⏸️️'
-                    : timerState.value.timestampPaused
-                      ? '▶️️ Resume'
-                      : '▶️️ Start'}
-            </button>
-            <button
-                onClick={resetTimer}
-                disabled={
-                    initialState.timerDuration === timerDuration.value
-                    && !timerState.value.timestampStarted
-                    && timerDurationRemaining.value !== 0
-                }
-                class={timerHasFinished.value ? 'highlighted' : ''}
-            >
-                🔁 Reset
-            </button>
-            <button
-                onClick={handleTimerCompletion}
-                disabled={
-                    timerHasFinished.value
-                    || timerState.value.currentPeriodIndex === null
-                    || timerDurationElapsed < 1 * 60 * 1000
-                }
-                class={
-                    timerOnLastPeriod.value && timerState.value.shouldGoToNextPeriod
-                        ? 'highlighted'
-                        : ''
-                }
-            >
-                🏁 Finish
-            </button>
+                <button
+                    onClick={handleTimerCompletion}
+                    disabled={
+                        timerHasFinished.value
+                        || timerState.value.currentPeriodIndex === null
+                        || timerDurationElapsed < 1 * 60 * 1000
+                    }
+                    class={
+                        timerOnLastPeriod.value && timerState.value.shouldGoToNextPeriod
+                            ? 'highlighted'
+                            : ''
+                    }
+                >
+                    🏁 Finish
+                </button>
+            </section>
+            <section class="controls">
+                <button
+                    onClick={moveToPreviousPeriod}
+                    disabled={
+                        timerHasFinished.value
+                        || timerState.value.currentPeriodIndex === null
+                        || timerState.value.currentPeriodIndex === 0
+                    }
+                >
+                    ⏮️
+                </button>
 
-            <button
-                onClick={moveToNextPeriod}
-                disabled={
-                    timerHasFinished.value
-                    || timerState.value.currentPeriodIndex === null
-                    || timerOnLastPeriod.value
-                }
-                class={
-                    !timerOnLastPeriod.value && timerState.value.shouldGoToNextPeriod
-                        ? 'highlighted'
-                        : ''
-                }
-            >
-                ⏭️
-            </button>
-        </section>
+                <button
+                    onClick={moveToNextPeriod}
+                    disabled={
+                        timerHasFinished.value
+                        || timerState.value.currentPeriodIndex === null
+                        || timerOnLastPeriod.value
+                    }
+                    class={
+                        !timerOnLastPeriod.value && timerState.value.shouldGoToNextPeriod
+                            ? 'highlighted'
+                            : ''
+                    }
+                >
+                    ⏭️
+                </button>
+            </section>
+        </>
     )
 }
